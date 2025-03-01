152e6d2e258f388e44d73b9945fae2fe88c7a4d1
Kuldeep Gothwal <kuldeepgothwal03@gmail.com>
2025-03-01 21:47:49 +0530

    setpu of first project

---
+++ surveilshield/celery.py
@@ -0,0 +1,8 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'surveilshield.settings')
+
+app = Celery('surveilshield')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
